56bf2b21a42626edaada2149d191b58e800e59bf
tmspnn <tmspnn@gmail.com>
2020-09-14 16:07:22 +0800

    Update page controller, templates and helper functions.

--- lua/controllers/page_controller.lua
+++ lua/controllers/page_controller.lua
@@ -1,4 +1,5 @@
 local to_json = require("lapis.util").to_json
+local util = require("./util")
 local user_controller = require("./controllers/user_controller")
 
 local page_controller = {}
@@ -7,39 +8,67 @@ local css_path = ""
 local js_path = ""
 local version = "1.0.0"
 
--- @required page.page_data: table
-local function set_page_attributes(page)
+local function handle_page(page, get_page_data)
+    -- page: lapis.Application
+    -- get_page_data: function(): table {
+    --   page_name,
+    --   js_path,
+    --   version,
+    --   page_title?,
+    --   tags_in_head?
+    -- }
+    local page_data = get_page_data(page)
+    local page_name_in_camel_case = util.camel_case(page_data.page_name)
     page.version = version
     page.css_path = css_path
     page.js_path = js_path
+    page.page_data = page_data
+    page.page_data_json_str = to_json(page_data)
+    page.page_name = page_name_in_camel_case
+    page.page_title = page_data.page_title
     page.tags_in_head = {{
         tag = "link",
         attributes = {
             type = "text/css",
             rel = "stylesheet",
-            href = css_path .. "/index-" .. version .. ".css"
+            href = css_path .. "/" .. page.page_name .. "-" .. version .. ".css"
         }
     }}
-    page.page_data_json_str = to_json(page.page_data)
+    return {
+        render = "pages." .. page_data.page_name
+    }
 end
 
--- @required v: string
+local function get_page_data_index(app)
+    local user_name = user_controller.get_user_name_by_token(app.cookies.token)
+    return {
+        page_name = "index",
+        page_title = "title for index.html",
+        user_name = user_name
+    }
+end
+
+local function get_page_data_sign_in(app)
+    local user_name = user_controller.get_user_name_by_token(app.cookies.token)
+    return {
+        page_name = "sign_in",
+        page_title = "title for signIn.html",
+        user_name = user_name
+    }
+end
+
 function page_controller.setVersion(v)
+    -- v: string
     version = v
 end
 
 function page_controller.bind(app)
     app:get("/", function(self)
-        self.page_name = "index"
-        local user_name = user_controller.get_user_name_by_token(self.cookies.token)
-        self.page_data = {
-            user_name = user_name,
-            title = "page index"
-        }
-        set_page_attributes(self)
-        return {
-            render = "pages.index"
-        }
+        return handle_page(self, get_page_data_index)
+    end)
+
+    app:get("/sign-in", function(self)
+        return handle_page(self, get_page_data_sign_in)
     end)
 end
 
--- lua/util.lua
+++ lua/util.lua
@@ -10,4 +10,16 @@ function util.get_table_keys(t)
     return keys
 end
 
+function util.camel_case(s)
+    return s:gsub("_(%w)", function(o)
+        return o:upper()
+    end)
+end
+
+function util.snake_case(s)
+    return s:gsub("[a-z]([A-Z])", function(o)
+        return "_" .. o:lower()
+    end)
+end
+
 return util
