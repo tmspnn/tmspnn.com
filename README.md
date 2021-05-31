285f5adcb0d742e5cb427179b8edf16a3b396c44
Thomas Peng <tmspnn@gmail.com>
2021-05-31 10:03:50 +0800

    Add editor.js

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # tmspnn.com
 
-Source code of [tmspnn.com](https://tmspnn.com)
-
-## Dependencies of the Lua code
-
-- basexx
-- bcrypt
-- date
-- lapis
-- penlight
-- pgmoon
-- sha1
-- resty.jit-uuid
-- resty.validation
-- resty.mail
-
-## Pg advisory locks (%d, %d), both are 32-bit integers
-
-- (0, $id). Rate an article by id
+Frontend & gateway of tmspnn.com
--- app/pages/editor/editor.js
+++ app/pages/editor/editor.js
@@ -1,5 +1,12 @@
 // External modules
 import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import LinkTool from "@editorjs/link";
+import ImageTool from "@editorjs/image";
+import CodeTool from "@editorjs/code";
+import InlineCode from "@editorjs/inline-code";
+import Quote from "@editorjs/quote";
 
 // Local modules
 import "./editor.scss";
@@ -14,7 +21,59 @@ const namespace = "editor";
 const root = new Page(namespace);
 root.editor = new EditorJS({
     holder: "editorjs",
-    placeholder: "写点什么吧..."
+    placeholder: "写点什么吧...",
+    tools: {
+        header: {
+            class: Header,
+            inlineToolbar: true,
+            shortcut: "CMD+SHIFT+H",
+            config: {
+                placeholder: "请输入标题",
+                levels: [2, 3, 4],
+                defaultLevel: 2
+            }
+        },
+        list: {
+            class: List,
+            inlineToolbar: true
+        },
+        linkTool: {
+            class: LinkTool,
+            config: {
+                endpoint: "/api/url-meta"
+            }
+        },
+        image: {
+            class: ImageTool,
+            config: {
+                uploader: {
+                    uploadByFile(file) {
+                        return MyAjax.upload(file).then(() => {
+                            return {
+                                success: 1,
+                                file: {
+                                    url: "https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg"
+                                }
+                            };
+                        });
+                    },
+                    uploadByUrl(url) {
+                        return MyAjax.upload(file).then(() => {
+                            return {
+                                success: 1,
+                                file: {
+                                    url: "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg"
+                                }
+                            };
+                        });
+                    }
+                }
+            }
+        },
+        code: CodeTool,
+        inlineCode: InlineCode,
+        quote: Quote
+    }
 });
 
 const ctrl = new PageController(namespace);
